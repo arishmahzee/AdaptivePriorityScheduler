28b5db0ef6b403478fa53739bae15843cfc29d2d
arishmahzee <arishmahzee@gmail.com>
2026-08-31 22:49:21 +0100

    Created and Modified TaskStatus.java - added Enum

--- src/main/java/AdaptivePriorityScheduler/tasks/TaskStatus.java
+++ src/main/java/AdaptivePriorityScheduler/tasks/TaskStatus.java
@@ -1,4 +1,11 @@
 package AdaptivePriorityScheduler.tasks;
 
 public class TaskStatus {
+    enum PRIORITY_LEVEL {
+        NOT_STARTED,
+        IN_PROGRESS,
+        DONE,
+        OVERDUE
+    }
+
 }
