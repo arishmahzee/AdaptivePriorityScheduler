8f3d451916dccbaee2279f9561b502db9b68aefa
arishmahzee <arishmahzee@gmail.com>
2026-08-31 20:58:46 +0100

    Created Task class

--- src/main/java/AdaptivePriorityScheduler/tasks/Task.java
+++ src/main/java/AdaptivePriorityScheduler/tasks/Task.java
@@ -1,4 +1,22 @@
-package AdaptivePriorityScheduler.Tasks;
+package AdaptivePriorityScheduler.tasks;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class Task {
+    private int id;
+    private String title;
+    private LocalDate deadline;
+    private float effortHours;
+    private String category;
+    private TaskStatus status;
+    private List<String> dependencyIds;
+
+    public Task(int idGiven, String titleGiven, LocalDate deadlineGiven,  float effortHoursGiven, String categoryGiven, TaskStatus status, List<String> dependencyIds) {
+
+    }
+
+    public String getSlack() {
+        return "getSlack";
+    }
 }
